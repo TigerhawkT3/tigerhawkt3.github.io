4333bda2e89d7a1dd772991317147ed251126129
David Muller <david@mullerhome.com>
2018-05-22 22:26:05 -0700

    In index.html - update main page news; tone.js - remove old commented code, simplify frequency value change, add changeBend and stopBend functions for bending; dviice.html - add pitch wheel, startBend, doBend, and stopBend for bending; style.css - add styling for pitch wheel canvas; help.html - add documentation for pitch wheel, slightly reorganize, clean up formatting.

--- dviice/v3/scripts/tone.js
+++ dviice/v3/scripts/tone.js
@@ -1,13 +1,7 @@
-
-// Example showing how to produce a tone using Web Audio API.
-//var oscillator;
-//var amp;
-//var modulatorOscillator;
-//var modulatorGain;
-
 var clickVoice; // click voice
 var voices = []; // chord voices
 var channels = []; // keyboard channels
+var bend = 1;
 // Create an oscillator and an amplifier.
 function initAudio() {
 	// Use audioContext from webaudio_tools.js
@@ -25,6 +19,7 @@ function Voice() {
     this.note = null;
     this.osc = audioContext.createOscillator();
     fixOscillator(this.osc);
+    this.frequency = 440;
     this.osc.frequency.value = 440;
     this.amp = audioContext.createGain();
     this.amp.gain.value = 0;
@@ -48,16 +43,25 @@ function Voice() {
     this.mosc.start();
     this.start = function(frequency, size=1) {
         var now = audioContext.currentTime;
-        this.osc.frequency.setValueAtTime(frequency, now);
+        this.frequency = frequency;
+        this.osc.frequency.value = this.frequency * bend;
         this.amp.gain.cancelScheduledValues(now);
         this.amp.gain.setValueAtTime(this.amp.gain.value, now);
-        this.amp.gain.linearRampToValueAtTime(0.6/(size+1), audioContext.currentTime + 0.1); // change to 'now'?
+        this.amp.gain.linearRampToValueAtTime(0.6/(size+1), now + 0.1);
     }
     this.stop = function() {
         var now = audioContext.currentTime;
         this.amp.gain.cancelScheduledValues(now);
         this.amp.gain.setValueAtTime(this.amp.gain.value, now);
-        this.amp.gain.linearRampToValueAtTime(0.0, audioContext.currentTime + 1.0); // change to 'now'?
+        this.amp.gain.linearRampToValueAtTime(0.0, now + 1.0);
+    }
+    this.changeBend = function(value) {
+        bend = 2**(value/12) // https://en.wikipedia.org/wiki/Scientific_pitch_notation#Table_of_note_frequencies
+        this.osc.frequency.value = this.frequency * bend
+    }
+    this.stopBend = function() {
+        this.osc.frequency.value = this.frequency;
+        bend = 1;
     }
     this.waveform = function(choice) {
         if (!choice) return this.osc.type
@@ -68,39 +72,5 @@ function Voice() {
         else this.mamp.gain.value = {'`':0, '1':5, '2':10, '3':15, '4':20, '5':25}[choice];
     }
 }
-/*// Set the frequency of the oscillator and start it running.
-function startTone(frequency, o=oscillator, a=amp, size=1) {
-	var now = audioContext.currentTime;
-	
-	o.frequency.setValueAtTime(frequency, now);
-	
-	// Ramp up the gain so we can hear the sound.
-	// We can ramp smoothly to the desired value.
-	// First we should cancel any previous scheduled events that might interfere.
-	a.gain.cancelScheduledValues(now);
-	// Anchor beginning of ramp at current value.
-	a.gain.setValueAtTime(a.gain.value, now);
-	a.gain.linearRampToValueAtTime(0.6/(size+1), audioContext.currentTime + 0.1);
-	
-	//writeMessageToID( "soundStatus", "<p>Play tone at frequency = " + frequency  + "</p>");
-}*/
-
-/*function stopTone(a=amp) {
-	var now = audioContext.currentTime;
-	a.gain.cancelScheduledValues(now);
-	a.gain.setValueAtTime(a.gain.value, now);
-	a.gain.linearRampToValueAtTime(0.0, audioContext.currentTime + 1.0);
-}*/
-/*function waveform(o=oscillator, i=null) {
-    // get or set waveform of given oscillator
-    if (!i) return o.type
-    o.type = {Q: 'sine', W: 'triangle', E: 'square', R: 'sawtooth'}[i]
-}*/
-
-/*function vibrato(g=modulatorGain, event=null) {
-    // get or set vibrato of given amplifier
-    if (!event) return (parseInt(g.gain.value / 5) || '`').toString();
-     g.gain.value = {'`':0, '1':5, '2':10, '3':15, '4':20, '5':25}[event];
-}*/
 // init once the page has finished loading.
 window.onload = initAudio;
